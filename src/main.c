0c4f769c03080c3d28585d320cdb8746ef189b0f
amir <ateymuri63@gmail.com>
2021-08-21 14:53:00 +0200

    interning symbols

--- src/main.c
+++ src/main.c
@@ -1,27 +1,44 @@
 #include <stdio.h>
+#include <string.h>
 
-char pret(char);
-void _strcpy_(char *);
+
+#define VOIDSYM ""		/* no symbol is allowed to be called this */
+
+typedef struct {
+  char *symname;
+  void *symvalue;
+} symbol;
+
+char *symnames[] = {
+  "pret", "name", VOIDSYM
+};
+
+size_t interened_symbols = 2;
+
+void intsym(symbol *s)
+{
+  int is_interened = 0, i = 0;
+  char *symname = s->symname;
+  while (strcmp(symnames[i], VOIDSYM))		/* is not the end */
+    if (!strcmp(symname, symnames[i])) {
+      is_interened = 1;
+      break;
+    } else i++;
+  if (is_interened)
+    printf("%s is interened\n", symname);
+  else {
+    printf("fresh symbol %s\n", symname);
+    symnames[interened_symbols] = symname;
+    interened_symbols++;
+    symnames[interened_symbols] = VOIDSYM;
+  }
+}
 
 int main()
 {
-  char *t = "Hello";
-  printf("====%p====%p\n", t, t+6);
-  _strcpy_(t);
-  /* printf("-> %s", t); */
+  symbol foo = {"FOO", "foo"};
+  intsym(&foo);
+  symbol bar = {"BAR", "bar"};
+  intsym(&bar);
   return 0;
-}
-
-char pret(char c)		/* Print and return c */
-{
-  printf("** %c **\n", c);
-  return c;
-}
-
-void _strcpy_(char *t)
-{
-  printf("====%p====.\n", t);
-  while (pret(*++t))
-    ;
-  printf("====%p====...\n", t);
 }
