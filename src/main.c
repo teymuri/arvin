25d84317ce12cc4c0e109f20f13411799f213615
amir <ateymuri63@gmail.com>
2021-08-31 14:08:02 +0200

    added lexparse.h/.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
-#define _GNU_SOURCE
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "let.h"
+#include <stdio.h>
+#include "lexparse.h"
 
 
 int main()
@@ -13,38 +11,4 @@ int main()
     printf("%zu %s", i, srclns[i]);
   free_srclns(n);
   exit(EXIT_SUCCESS);
-}
-
-/*
- free srclns 
-*/
-void free_srclns(size_t n)
-{
-  for (size_t i = 0; i < n; i++)
-    free(srclns[i]);
-}
-
-size_t read_lines(char *path)
-{
-  FILE *stream;
-  ssize_t read;
-  char *lnptr = NULL;
-  size_t n = 0;
-  size_t count = 0;
-  stream = fopen(path, "r");
-  if (!stream) {
-    fprintf(stderr, "can't open source '%s'\n", path);
-    exit(EXIT_FAILURE);
-  }
-  while ((read = getline(&lnptr, &n, stream)) != -1) {
-    /*
-     * jump over the line if it begins with a newline char (dismissing empty lines)
-     */
-    if (*lnptr != '\n')
-      srclns[count++] = lnptr;
-    lnptr = NULL;
-  }
-  free(lnptr);
-  fclose(stream);
-  return count;
 }
