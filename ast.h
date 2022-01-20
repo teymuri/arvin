2dfab486742e669320cdcb2ca45b49906493e75c
amir@bunsenlabs <teymuriworks@gmail.com>
2022-01-20 15:18:59 +0100

    removed deprecated function decls from ast.h

--- ast.h
+++ ast.h
@@ -3,8 +3,5 @@
 
 void ascertain_lambda_spellings(GNode *root);
 GNode *parse3(GSList *atoms);
-struct Cons **parse__Hp(struct Cons *global_block, struct Unit *linked_cells_root, int *blocks_count);
-void amend_lambda_semantics(struct Cons *);
-void free_parser_blocks(struct Cons **blocks, int blocks_count);
 
 #endif	/* LET_AST_H */
